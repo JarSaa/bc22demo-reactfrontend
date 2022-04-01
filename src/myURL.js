419fbb5866581cf7bb4797aaf8e0f42a455ff052
Jari Sääksi <jari.saaksi@gmail.com>
2022-04-01 16:14:28 +0300

    Toimisiko ja Herokussa, toinen yritys URL korjattu

--- src/myURL.js
+++ src/myURL.js
@@ -1,5 +1,5 @@
 //const apiURL="http://localhost:3001";
-const apiURL="https://github.com/JarSaa/bc22demo-reactfrontend.git";
+const apiURL="https://dashboard.heroku.com/apps/bc2020-postgre-api";
 //const apiURL="https://yourappname.herokuapp.com";
 
 export default apiURL;
