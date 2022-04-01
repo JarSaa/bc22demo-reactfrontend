031bbdf662833085c1c5656352dfe8f73ea019bc
Jari Sääksi <jari.saaksi@gmail.com>
2022-04-01 16:21:19 +0300

    Toimisiko ja Herokussa, kolmas yritys URL korjattu

--- src/myURL.js
+++ src/myURL.js
@@ -1,5 +1,5 @@
 //const apiURL="http://localhost:3001";
-const apiURL="https://dashboard.heroku.com/apps/bc2020-postgre-api";
+const apiURL="https://bc2020-postgre-api.herokuapp.com";
 //const apiURL="https://yourappname.herokuapp.com";
 
 export default apiURL;
