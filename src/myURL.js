0ff66016802ff38fc038c36489236c50657102a8
Jari Sääksi <jari.saaksi@gmail.com>
2022-04-01 16:09:15 +0300

    Toimisiko ja Herokussa

--- src/myURL.js
+++ src/myURL.js
@@ -1,4 +1,5 @@
-const apiURL="http://localhost:3001";
+//const apiURL="http://localhost:3001";
+const apiURL="https://github.com/JarSaa/bc22demo-reactfrontend.git";
 //const apiURL="https://yourappname.herokuapp.com";
 
 export default apiURL;
